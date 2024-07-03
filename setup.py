c7e246e5a43dbe0c872b5553dfaee37bbc84f59c
yaotiankuo <yaotiankuo@sensetime.com>
2024-07-03 15:54:26 +0800

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mytools',
+      version='0.1',
+      description='work tools',
+      url='https://github.com/moolean/workTools.git',
+      author='yao tiankuo',
+      author_email='yaotiankuo@sensetime.com',
+      license='MIT',
+      packages=['mytools'],
+      zip_safe=False)
